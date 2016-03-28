50953138c9ae4a7c3d15933b03f5870bca4be75e
Shmuel Kallner <kallner@il.ibm.com>
2016-03-28 13:04:54 +0300

    IBM-Swift/Kitura#327 Update to latest develop branch of Kitura

--- Package.swift
+++ Package.swift
@@ -25,10 +25,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 0)
     ],
-    exclude: ["Makefile", "Kitura-CI"])
+    exclude: ["Makefile", "Kitura-Build"])
 
