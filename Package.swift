ad517a592dc9633c09e5f74889a3174cb1a01464
Vadim Eisenberg <vadime@il.ibm.com>
2016-04-09 16:20:55 +0300

    IBM-Swift/Kitura#340 updated dependency on Kitura-MustacheTemplateEngine

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 0)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 7)
     ],
     exclude: ["Makefile", "Kitura-Build"])
 
