80af35562708b92ace6519488fb47019b2b1014f
Daniel Firsht <dfirsht@us.ibm.com>
2016-04-29 10:29:32 -0500

    Added mustache example back in

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 11),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 6),
-        //.Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 9),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-MustacheTemplateEngine.git", majorVersion: 0, minor: 12),
     ],
     exclude: ["Makefile", "Kitura-Build"])
 
